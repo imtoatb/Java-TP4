bcb64ed187e4349800780bf0cb1fe7191d8db077
imtoatb <idfc.gfy@gmail.com>
2025-04-11 16:19:52 +0200

    Java TP4 Exo 1

---
+++ src/MultipleOf.java
@@ -0,0 +1,24 @@
+public class MultipleOf implements Condition{
+    private int n;
+
+    public MultipleOf(int n){
+        this.n = n;
+    }
+
+    @Override
+    public String toString(){
+        return("(Multiple of " + n + ")");
+    }
+
+    public boolean verif(int nb){
+        return (nb % n == 0);
+    }
+
+    public String result(int nb){
+        if (verif(nb)){
+            return (nb + " is a multiple of " + n);
+        } else {
+            return (nb + " is not a multiple of " + n);
+        }
+    }
+}
